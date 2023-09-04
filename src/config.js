b3733b32fabbdcb9c034c6250536ccf29472871b
sahanilxm <laxmisahani240@gmail.com>
2023-09-04 09:51:50 +0530

    CORS added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
-export const API_URL= "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139&lng=77.2090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const API_URL= "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139&lng=77.2090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const IMG_CDN_URL= "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
